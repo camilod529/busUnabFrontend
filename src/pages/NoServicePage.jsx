7a18a9603f6fae5d29d315e0655cdcff5daf9ad8
CamiloPC <camilod529@gmail.com>
2023-08-19 07:47:30 -0500

    cambiado logo bus pagina no servicio

--- src/pages/NoServicePage.jsx
+++ src/pages/NoServicePage.jsx
@@ -1,15 +1,12 @@
 import React from "react";
+import busLogo from "../assets/logo-busu.svg";
 
 export const NoServicePage = () => {
   return (
     <>
       <div className="row text-center gap-2" style={{ height: "100vh", width: "100vw" }}>
         <div className="col align-self-center">
-          <img
-            src="../../static/svg/logo-busu.svg"
-            alt="UNAB logo"
-            style={{ height: "15rem", width: "auto" }}
-          />
+          <img src={busLogo} alt="UNAB logo" style={{ height: "15rem", width: "auto" }} />
           <p className="fs-5 mt-4">
             <b>
               ¡Importante! Ten en cuenta que los fines de semana no se presta el servicio de ruta
