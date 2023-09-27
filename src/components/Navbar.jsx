bf24dca3822b42dd9ab21e5bfe3807f1a7716275
chokpikLaptop <camilod529@gmail.com>
2023-09-27 11:17:17 -0500

    Cambiado manejo de la ruta en el navbar para que sea compatible con el endpoint de rutas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { useDispatch } from "react-redux";
 import { changeRoute } from "../store/route/routeSlice";
 
@@ -5,6 +7,15 @@ import "../css/navbar.css";
 
 export const Navbar = () => {
   const dispatch = useDispatch();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://bus.unab.edu.co/control/api/routes/")
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response);
+      });
+  }, []);
 
   return (
     <>
@@ -17,56 +28,58 @@ export const Navbar = () => {
               style={{ height: "2em" }}
             />
           </span>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Rutas
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li>
-                    <div
-                      className="dropdown-item"
-                      onClick={() => {
-                        dispatch(changeRoute(1));
-                        localStorage.setItem("route", 1);
-                      }}
+
+          {/* Routes menu */}
+          {data.length > 0 ? (
+            <>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      id="navbarDropdownMenuLink"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
                     >
-                      Ruta 1
-                    </div>
-                  </li>
-                  <li>
-                    <div
-                      className="dropdown-item"
-                      onClick={() => {
-                        dispatch(changeRoute(2));
-                        localStorage.setItem("route", 2);
-                      }}
-                    >
-                      Ruta 2
-                    </div>
+                      Rutas
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      {/* Duplicar para estaciones */}
+                      {data.map((route, index) => {
+                        return (
+                          <li key={route.name}>
+                            <div
+                              className="dropdown-item"
+                              onClick={() => {
+                                dispatch(changeRoute(index + 1));
+                                localStorage.setItem("route", index + 1);
+                              }}
+                            >
+                              <span>{route.name}</span>
+                            </div>
+                          </li>
+                        );
+                      })}
+                    </ul>
                   </li>
                 </ul>
-              </li>
-            </ul>
-          </div>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </nav>
     </>
