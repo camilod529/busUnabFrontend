6d0a0fdb64db0221335b604ad52c2a59ed71e7f8
CamiloPC <camilod529@gmail.com>
2023-11-07 18:23:57 -0500

    user location for center and user location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -25,6 +25,7 @@ const busMarkers: BusMarkers = {};
 export const Map = () => {
   const [stops, setStops] = useState<Stop[]>([]);
   const [selectedStop, setSelectedStop] = useState<Stop | null>(null);
+  const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral | null>(null);
 
   const route = useSelector((state: RootState) => state.route.route);
   const buses: BusesState = useSelector((state: RootState) => state.buses);
@@ -39,6 +40,29 @@ export const Map = () => {
     }
   );
 
+  // geolocation
+  useEffect(() => {
+    let watchID: number;
+    if ("geolocation" in navigator) {
+      watchID = navigator.geolocation.watchPosition(
+        function (position) {
+          const userPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setUserLocation(userPosition);
+        },
+        () => {},
+        { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+      );
+    } else {
+      console.log("No se puede obtener la ubicación");
+    }
+    return () => {
+      navigator.geolocation.clearWatch(watchID);
+    };
+  }, []);
+  console.log(userLocation);
   useEffect(() => {
     // Realiza la animación de los marcadores de los autobuses
     if (lastJsonMessage && lastJsonMessage.message.route == route) {
@@ -79,7 +103,7 @@ export const Map = () => {
         zIndex: 9,
       }}
       zoom={16}
-      center={center}
+      center={userLocation ? userLocation : center}
       mapTypeId="roadmap"
       options={{
         fullscreenControl: false,
@@ -92,6 +116,19 @@ export const Map = () => {
         gestureHandling: "greedy",
       }}
     >
+      // * User location
+      {userLocation && (
+        <MarkerF
+          position={userLocation}
+          icon={{
+            url: "https://cdn0.iconfinder.com/data/icons/user-icons-4/100/user-17-512.png",
+            anchor: new google.maps.Point(17, 46),
+            scaledSize: new google.maps.Size(50, 50),
+            labelOrigin: new google.maps.Point(20, 65),
+          }}
+          zIndex={10}
+        />
+      )}
       {/* Bus marker */}
       {Object.keys(buses).length > 0 &&
         Object.keys(buses).map((bus: string) => {
@@ -118,7 +155,6 @@ export const Map = () => {
             />
           );
         })}
-
       {/* StopMarker */}
       {stops?.map((stop) => {
         return (
