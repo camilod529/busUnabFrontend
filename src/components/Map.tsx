0877393c11e3a7437e75efe0cfe49eeabce5eb21
camilod529 <camilod529@gmail.com>
2023-11-08 16:51:52 -0500

    Minor changes to deploy

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -25,7 +25,6 @@ const busMarkers: BusMarkers = {};
 export const Map = () => {
   const [stops, setStops] = useState<Stop[]>([]);
   const [selectedStop, setSelectedStop] = useState<Stop | null>(null);
-  const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral | null>(null);
 
   const route = useSelector((state: RootState) => state.route.route);
   const buses: BusesState = useSelector((state: RootState) => state.buses);
@@ -40,28 +39,6 @@ export const Map = () => {
     }
   );
 
-  // geolocation
-  useEffect(() => {
-    let watchID: number;
-    if ("geolocation" in navigator) {
-      watchID = navigator.geolocation.watchPosition(
-        function (position) {
-          const userPosition = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          setUserLocation(userPosition);
-        },
-        () => {},
-        { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
-      );
-    } else {
-      console.log("No se puede obtener la ubicación");
-    }
-    return () => {
-      navigator.geolocation.clearWatch(watchID);
-    };
-  }, []);
   useEffect(() => {
     // Realiza la animación de los marcadores de los autobuses
     if (lastJsonMessage && lastJsonMessage.message.route == route) {
@@ -102,7 +79,7 @@ export const Map = () => {
         zIndex: 9,
       }}
       zoom={16}
-      center={userLocation ? userLocation : center}
+      center={center}
       mapTypeId="roadmap"
       options={{
         fullscreenControl: false,
@@ -115,19 +92,6 @@ export const Map = () => {
         gestureHandling: "greedy",
       }}
     >
-      // * User location
-      {userLocation && (
-        <MarkerF
-          position={userLocation}
-          icon={{
-            url: "https://cdn0.iconfinder.com/data/icons/user-icons-4/100/user-17-512.png",
-            anchor: new google.maps.Point(17, 46),
-            scaledSize: new google.maps.Size(50, 50),
-            labelOrigin: new google.maps.Point(20, 65),
-          }}
-          zIndex={10}
-        />
-      )}
       {/* Bus marker */}
       {Object.keys(buses).length > 0 &&
         Object.keys(buses).map((bus: string) => {
